a215a2904fa7747163f33c910aaeb6856d20e4fe
Aries <aho11@stuy.edu>
2020-11-01 18:51:55 -0500

    added and implemented add method

--- SuperArray.java
+++ SuperArray.java
@@ -13,4 +13,11 @@ public class SuperArray{
     }
     return s;
   }
+
+  public boolean add(String element){
+    for(int i = 0; i < data.length; i++){
+      if(data[i] == null) data[i] = element;
+    }
+    return true;
+  }
 }
