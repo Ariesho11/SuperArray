f18c5fcb4df661a40951a31415f3a7c9cafa4deb
Aries <aho11@stuy.edu>
2020-11-07 02:04:38 -0500

    findOverlap seems to work

--- Demo.java
+++ Demo.java
@@ -7,6 +7,16 @@ public class Demo{
     }
   }
 
+  public static SuperArray findOverlap(SuperArray a, SuperArray b){
+    SuperArray overlap = new SuperArray();
+    for(int i = 0; i < a.size(); i++){
+      for(int j = 0; j < b.size()-1; j++){
+        if (a.get(i).equals(b.get(j))) overlap.add(a.get(i));
+      }
+    }
+    return overlap;
+  }
+
   public static void main(String[]args){
     SuperArray words = new SuperArray();
     //grouped to save vertical space
@@ -16,5 +26,17 @@ public class Demo{
 
     System.out.println(words);
     removeDuplicates(words);
-    System.out.println(words);  }
+    System.out.println(words);
+
+    SuperArray t1 = new SuperArray();
+    SuperArray t2 = new SuperArray();
+    for(int i = 1; i < 11; i++){
+      t1.add(String.valueOf(i));
+      t2.add(String.valueOf(i*2));
+    }
+    System.out.println("t1: "+ t1);
+    System.out.println("t2: "+ t2);
+    System.out.println(findOverlap(t1, t2));
+
+    }
 }
